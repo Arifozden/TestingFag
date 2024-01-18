bfdef0689ef505fba04cae6b525851e46422b869
arifozden <arifozden1@gmail.com>
2024-01-18 14:31:38 +0100

    km sjekk added

--- src/test/Bil.java
+++ src/test/Bil.java
@@ -3,11 +3,30 @@ package test;
 public class Bil {
     private String farge;
 
+    private int km;
+
     public String getFarge() {
         return farge;
     }
 
     public void setFarge(String farge) {
         this.farge = farge;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public String sjekkService(){
+        if(km < 20000){
+            return "Service er ikke nødvendig";
+        } else if (km >= 20000 && km < 50000) {
+            return "20000 km service er nødvendig";
+        }
+        return "50000 km service er nødvendig";
     }
 }
--- src/test/BilTest.java
+++ src/test/BilTest.java
@@ -12,4 +12,25 @@ public class BilTest {
         assertEquals("Rød",farge);
     }
 
+    @Test
+    public void testKm1(){
+        Bil volvo = new Bil();
+        volvo.setKm(10000);
+        assertEquals("Service er ikke nødvendig", volvo.sjekkService());
+    }
+
+    @Test
+    public void testKm2(){
+        Bil volvo = new Bil();
+        volvo.setKm(21000);
+        assertEquals("20000 km service er nødvendig", volvo.sjekkService());
+    }
+
+    @Test
+    public void testKm3(){
+        Bil volvo = new Bil();
+        volvo.setKm(51000);
+        assertEquals("50000 km service er nødvendig", volvo.sjekkService());
+    }
+
 }
